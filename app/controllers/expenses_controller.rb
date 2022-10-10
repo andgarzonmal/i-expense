fa2c6455fadfff741effb11e43d6224cee8d2c23
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-10 10:08:18 -0500

    Add current user to create expense action

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -22,7 +22,7 @@ class ExpensesController < ApplicationController
   # POST /expenses or /expenses.json
   def create
     @expense = Expense.new(expense_params)
-
+    @expense.user = current_user
     respond_to do |format|
       if @expense.save
         format.html { redirect_to expense_url(@expense), notice: "Expense was successfully created." }
