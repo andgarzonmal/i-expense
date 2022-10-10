2a8d0069fd4154752fb8e342ea3b0a860e954020
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-10 14:33:17 -0500

    Add splash page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :groups
   resources :expenses
-  devise_for :users
+  devise_for :users, path: ''
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
 end
