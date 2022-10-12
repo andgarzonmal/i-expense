7e7a33c129ddfd5f804803c6b0ec6cb9126576f8
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-12 10:22:38 -0500

    Add helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def category_sum(group)
+    total = 0
+    return total if group.expenses.empty?
+
+    group.expenses.each do |ex|
+      total += ex.amount
+    end
+    total
+  end
 end
