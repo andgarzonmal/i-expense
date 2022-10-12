47dd45607c7d1e40197154f211ab01e5cf7e62bf
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-12 10:24:34 -0500

    Configure routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :groups
-  resources :expenses
   devise_for :users, path: ''
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
+  resources :groups do
+    resources :expenses
+  end
   root "splash#index"
 end
